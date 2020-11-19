64011877d1ae36340267238ae501caa5315e2783
tavenli <tavenli@qq.com>
2020-11-19 17:16:03 +0800

    -

--- GoBase/go.mod
+++ GoBase/go.mod
@@ -1,3 +1,8 @@
 module GoBase
 
 go 1.15
+
+require (
+	github.com/json-iterator/go v1.1.10
+	github.com/ugorji/go/codec v1.2.0
+)
--- GoBase/main.go
+++ GoBase/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"os"
+	"reflect"
+	"runtime"
 	"runtime/debug"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -19,9 +24,13 @@ func main() {
 	fmt.Println("拼接成字符串2：" + note)
 	fmt.Println("---------------")
 
+	ShowSysInf()
+
 	GoLangInitObj()
 
 	demo_string()
+
+	fmt.Println("==========================")
 }
 
 type UserData struct {
@@ -255,4 +264,116 @@ func demo_string() {
 	fmt.Println(" SplitAfterN 函数的用法")
 	fmt.Printf("%q\n", strings.SplitAfterN("/home/m_ta/src", "/", 2)) //["/"
 
+}
+
+func demo_collection() {
+	//集合对象的操作
+
+}
+
+//	显示当前系统基本信息
+func ShowSysInf() {
+
+	fmt.Println("★★★★★★★★★★★★★★★★★★★★★★★★")
+	fmt.Println("runtime.Version --->", runtime.Version()) //GO的版本
+	fmt.Println("runtime.NumCPU --->", runtime.NumCPU())   //CPU核数
+	fmt.Println("runtime.GOOS --->", runtime.GOOS)         //运行GO的OS操作系统
+	fmt.Println("runtime.GOARCH --->", runtime.GOARCH)     //CPU架构
+	fmt.Println("runtime.Version --->", runtime.Version()) //当前GO语言版本
+	fmt.Println("time --->", time.Now())                   //系统当前时间
+	fmt.Println("★★★★★★★★★★★★★★★★★★★★★★★★")
+
+	//var memStats runtime.MemStats
+	//runtime.ReadMemStats(&memStats)
+	//fmt.Println("runtime.memStats --->", memStats)
+
+	//获取全部的环境变量
+	// data := os.Environ()
+	// for _, val := range data {
+	//     fmt.Println(val)
+	// }
+
+}
+
+//	go不支持三元表达式，可以使用自定义的函数实现
+//	例如：max := utils.If(x > y, x, y).(int)
+func If(condition bool, trueVal, falseVal interface{}) interface{} {
+
+	if condition {
+		return trueVal
+	}
+	return falseVal
+}
+
+/*
+	交换int数据：a, b := utils.Swap(2, 9)
+	交换字符串数据：A, B := utils.Swap("Li", "Chen")
+*/
+func Swap(x, y interface{}) (interface{}, interface{}) {
+	return y, x
+}
+
+//	设置环境变量
+func SetEnv(key, value string) error {
+
+	return os.Setenv(key, value)
+}
+
+//	取环境变量的值
+func GetEnv(key string) string {
+
+	return os.Getenv(key)
+}
+
+//取进程ID
+func GetPid() int {
+	return os.Getpid()
+}
+
+func KillByPid(pid int) {
+	p, _ := os.FindProcess(pid)
+	fmt.Println("KillByPid", p)
+	p.Kill()
+}
+
+func StartProcessDemo() {
+	//例子演示
+	attr := &os.ProcAttr{
+		Files: []*os.File{os.Stdin, os.Stdout, os.Stderr},
+	}
+	p, _ := os.StartProcess("xxx.exe", []string{"xxx", "1.txt"}, attr)
+	p.Release()
+	time.Sleep(10000)
+	p.Signal(os.Kill)
+	os.Exit(10)
+}
+
+func ToJson(obj interface{}) ([]byte, error) {
+	return json.Marshal(obj)
+}
+
+func FromJson(data []byte, t interface{}) error {
+	return json.Unmarshal(data, t)
+}
+
+func ShowObjAllProp(obj interface{}) {
+	value_method := reflect.ValueOf(obj)
+	obj_type := value_method.Type()
+
+	fmt.Printf("输出对象的属性和方法\t%v\n", obj)
+
+	fmt.Println("\tMethods...")
+
+	for i := 0; i < value_method.NumMethod(); i++ {
+		fmt.Printf("\t%d\t%s\n", i, obj_type.Method(i).Name)
+	}
+
+	value_element := reflect.ValueOf(obj).Elem()
+	obj_element := value_element.Type()
+
+	fmt.Println("\tFields...")
+	for i := 0; i < value_element.NumField(); i++ {
+		fmt.Printf("\t%d\t%s\n", i, obj_element.Field(i).Name)
+
+	}
 }
