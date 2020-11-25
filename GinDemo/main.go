bb7323f06222d4b03dbb0f647afe686dd836006e
tavenli <tavenli@qq.com>
2020-11-25 17:18:08 +0800

    -

--- GinDemo/main.go
+++ GinDemo/main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("------------")
+
+	// 禁用控制台颜色
+	gin.DisableConsoleColor()
+
+	// 创建记录日志的文件
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f)
+
+	// 如果需要将日志同时写入文件和控制台，请使用以下代码
+	// gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
 
 	router := gin.Default()
 	router.Static("/static", "./static")
