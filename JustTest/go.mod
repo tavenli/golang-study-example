0bb417ed334717c4a6d0320a217410eaf3e86cf7
tavenli <tavenli@qq.com>
2021-02-20 12:44:16 +0800

    -

---
+++ JustTest/go.mod
@@ -0,0 +1,5 @@
+module JustTest
+
+go 1.15
+
+require github.com/unidoc/unipdf/v3 v3.19.0
--- JustTest/main.go
+++ JustTest/main.go
@@ -1,10 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/unidoc/unipdf/v3/extractor"
+	pdf "github.com/unidoc/unipdf/v3/model"
+	"os"
+)
 
 func main() {
 	fmt.Println("开始测试")
 
+	outputPdfText("d:\\test.pdf")
 
 	fmt.Println("结束测试")
+}
+
+func outputPdfText(inputPath string) error {
+	f, err := os.Open(inputPath)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	pdfReader, err := pdf.NewPdfReader(f)
+	if err != nil {
+		return err
+	}
+
+	numPages, err := pdfReader.GetNumPages()
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("--------------------\n")
+	fmt.Printf("PDF to text extraction:\n")
+	fmt.Printf("--------------------\n")
+	for i := 0; i < numPages; i++ {
+		pageNum := i + 1
+
+		page, err := pdfReader.GetPage(pageNum)
+		if err != nil {
+			return err
+		}
+
+		ex, err := extractor.New(page)
+		if err != nil {
+			return err
+		}
+
+		text, err := ex.ExtractText()
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("------------------------------")
+		fmt.Printf("Page %d:\n", pageNum)
+		fmt.Printf("\"%s\"\n", text)
+		fmt.Println("------------------------------")
+	}
+
+	return nil
 }
