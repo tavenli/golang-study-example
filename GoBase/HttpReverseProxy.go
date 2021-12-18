6dec2b769ed9428c78d80993d482e54edb8a15d1
TavenLi <tavenli@qq.com>
2021-12-18 16:59:36 +0800

    -

---
+++ GoBase/HttpReverseProxy.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+func ReverseProxy_main()  {
+
+	u, err := url.Parse("http://localhost:8080/test")
+	if err != nil {
+		log.Fatal("url.Parse: %v", err)
+	}
+	proxy := httputil.NewSingleHostReverseProxy(u)
+	log.Printf("Listening at :8081")
+	if err := http.ListenAndServe(":8081", proxy); err != nil {
+		log.Fatal("ListenAndServe: %v", err)
+	}
+
+}
+
+
+func NewMultipleHostsReverseProxy(targets []*url.URL) *httputil.ReverseProxy {
+	director := func(req *http.Request) {
+		target := targets[rand.Int()%len(targets)]
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+		req.URL.Path = target.Path
+	}
+	//反向代理
+	return &httputil.ReverseProxy{Director: director}
+}
+
+func ReverseProxy_main2() {
+	proxy := NewMultipleHostsReverseProxy([]*url.URL{
+		{
+			Scheme: "http",
+			Host:   "localhost:9091",
+		},
+		{
+			Scheme: "http",
+			Host:   "localhost:9092",
+		},
+	})
+	log.Fatal(http.ListenAndServe(":9090", proxy))
+}
