f6c89342d48b33fb87f8dc118a2ae1533f03e680
TavenLi <tavenli@qq.com>
2023-11-29 10:14:01 +0800

    -

--- GoBase/HtmlTemplate_Demo.go
+++ GoBase/HtmlTemplate_Demo.go
@@ -10,6 +10,40 @@ import (
 	"time"
 )
 
+/*
+	var builtins = FuncMap{
+	    // 返回第一个为空的参数或最后一个参数。可以有任意多个参数。
+	    // "and x y"等价于"if x then y else x"
+	    "and": and,
+	    // 显式调用函数。第一个参数必须是函数类型，且不是template中的函数，而是外部函数。
+	    // 例如一个struct中的某个字段是func类型的。
+	    // "call .X.Y 1 2"表示调用dot.X.Y(1, 2)，Y必须是func类型，函数参数是1和2。
+	    // 函数必须只能有一个或2个返回值，如果有第二个返回值，则必须为error类型。
+	    "call": call,
+	    // 返回与其参数的文本表示形式等效的转义HTML。
+	    // 这个函数在html/template中不可用。
+	    "html": HTMLEscaper,
+	    // 对可索引对象进行索引取值。第一个参数是索引对象，后面的参数是索引位。
+	    // "index x 1 2 3"代表的是x[1][2][3]。
+	    // 可索引对象包括map、slice、array。
+	    "index": index,
+	    // 返回与其参数的文本表示形式等效的转义JavaScript。
+	    "js": JSEscaper,
+	    // 返回参数的length。
+	    "len": length,
+	    // 布尔取反。只能一个参数。
+	    "not": not,
+	    // 返回第一个不为空的参数或最后一个参数。可以有任意多个参数。
+	    // "or x y"等价于"if x then x else y"。
+	    "or":      or,
+	    "print":   fmt.Sprint,
+	    "printf":  fmt.Sprintf,
+	    "println": fmt.Sprintln,
+	    // 以适合嵌入到网址查询中的形式返回其参数的文本表示的转义值。
+	    // 这个函数在html/template中不可用。
+	    "urlquery": URLQueryEscaper,
+	}
+*/
 func HtmlTemplate_main() {
 
 	//
