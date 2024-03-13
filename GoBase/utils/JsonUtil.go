6e4a15c7a5027e61fc75c9ceeaafe96287ccdf04
TavenLi <tavenli@qq.com>
2024-03-13 09:22:25 +0800

    -

--- GoBase/utils/JsonUtil.go
+++ GoBase/utils/JsonUtil.go
@@ -6,6 +6,10 @@ func ToJson(obj interface{}) ([]byte, error) {
 	return json.Marshal(obj)
 }
 
+func ToPrettyJson(obj interface{}) ([]byte, error) {
+	return json.MarshalIndent(obj, "", "	")
+}
+
 func FromJson(data []byte, t interface{}) error {
 	return json.Unmarshal(data, t)
 }
