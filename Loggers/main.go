d4b2412b51af269300b1cc54526f996295709fc0
Taven.Li <tavenli@qq.com>
2025-01-09 22:49:33 +0800

    -

--- Loggers/main.go
+++ Loggers/main.go
@@ -4,11 +4,13 @@ import (
 	"Loggers/zlogger"
 	"errors"
 	"fmt"
-	"github.com/bytedance/sonic"
 	"github.com/logrusorgru/aurora"
 	"github.com/projectdiscovery/gologger"
 	"github.com/projectdiscovery/gologger/levels"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"io"
+	"os"
 	"strconv"
 	"time"
 )
@@ -22,13 +24,17 @@ func main() {
 	fmt.Println(aurora.BrightYellow("Loggers_Demo"))
 	fmt.Println(aurora.BrightMagenta("Loggers_Demo"))
 
-	//
-	json_sonic()
+	logger_demo1()
 
-	//logger_demo1()
+	zap_logger_demo1()
 
-	zlogger_demo1()
+	zap_logger_demo2()
 
+	zap_logger_demo3()
+
+	zap_logger_demo4()
+
+	zap_logger_demo5()
 }
 
 func logger_demo1() {
@@ -67,7 +73,9 @@ func logger_demo1() {
 	fmt.Println("----------------")
 }
 
-func zlogger_demo1() {
+func zap_logger_demo1() {
+
+	fmt.Println("zap_logger_demo1")
 	action := "do loging"
 	zlogger.Info("开始运行", time.Now())
 	zlogger.ChangeSilent(false)
@@ -82,29 +90,119 @@ func zlogger_demo1() {
 	zlogger.Error("err:", zap.Error(err))
 	zlogger.Errors("Logger error", err)
 
+	fmt.Println("----------------")
+
 	defer zlogger.Sync()
 }
 
-type DemoStruct struct {
-	AccessKey string `json:"access_key"`
-	Data      string `json:"data"`
+func zap_logger_demo2() {
+	fmt.Println("zap_logger_demo2")
+	// The bundled Config struct only supports the most common configuration
+	// options. More complex needs, like splitting logs between multiple files
+	// or writing to non-file outputs, require use of the zapcore package.
+	//
+	// In this example, imagine we're both sending our logs to Kafka and writing
+	// them to the console. We'd like to encode the console output and the Kafka
+	// topics differently, and we'd also like special treatment for
+	// high-priority logs.
+
+	// First, define our level-handling logic.
+	highPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= zapcore.ErrorLevel
+	})
+	lowPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl < zapcore.ErrorLevel
+	})
+
+	// Assume that we have clients for two Kafka topics. The clients implement
+	// zapcore.WriteSyncer and are safe for concurrent use. (If they only
+	// implement io.Writer, we can use zapcore.AddSync to add a no-op Sync
+	// method. If they're not safe for concurrent use, we can add a protecting
+	// mutex with zapcore.Lock.)
+	topicDebugging := zapcore.AddSync(io.Discard)
+	topicErrors := zapcore.AddSync(io.Discard)
+
+	// High-priority output should also go to standard error, and low-priority
+	// output should also go to standard out.
+	consoleDebugging := zapcore.Lock(os.Stdout)
+	consoleErrors := zapcore.Lock(os.Stderr)
+
+	// Optimize the Kafka output for machine consumption and the console output
+	// for human operators.
+	kafkaEncoder := zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
+	consoleEncoder := zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
+
+	// Join the outputs, encoders, and level-handling functions into
+	// zapcore.Cores, then tee the four cores together.
+	core := zapcore.NewTee(
+		zapcore.NewCore(kafkaEncoder, topicErrors, highPriority),
+		zapcore.NewCore(consoleEncoder, consoleErrors, highPriority),
+		zapcore.NewCore(kafkaEncoder, topicDebugging, lowPriority),
+		zapcore.NewCore(consoleEncoder, consoleDebugging, lowPriority),
+	)
+
+	// From a zapcore.Core, it's easy to construct a Logger.
+	logger := zap.New(core)
+	defer logger.Sync()
+
+	logger.Debug("constructed a logger")
+	logger.Info("hi boys!")
+	logger.Error("err:", zap.Error(errors.New("something wrong")))
+
+	fmt.Println("----------------")
 }
 
-func json_sonic() {
-	var demoStruct DemoStruct
-
-	payload := `{"access_key":"abc","data":"sonic hello"}`
-	//payload := `{"access_key":"abc","secret_key":"123456","creator":"root2","creator_id":0}`
-	//payload := `{"access_key":"abc","secret_key":"123456","creator":"root2","creator_id":0,}`
-
-	// 这里没有进行输入验证，导致反序列化漏洞
-	err := sonic.Unmarshal([]byte(payload), &demoStruct)
+func zap_logger_demo3() {
+	fmt.Println("zap_logger_demo3")
+	logger, err := zap.NewDevelopment()
 	if err != nil {
-		//zlogger.Errors("Logger error", err)
-		fmt.Println(err)
 		return
 	}
 
-	fmt.Println(demoStruct.Data)
+	type foo struct {
+		One string
+		Two string
+	}
 
+	logger.Debug("zap_logger_demo3", zap.Any("foo", foo{One: "one", Two: "two"}))
+	logger.Info("zap_logger_demo3", zap.Any("foo", foo{One: "one", Two: "two"}))
+	//logger.Warn("zap_logger_demo3", zap.Any("foo", foo{One: "one", Two: "two"}))
+	//logger.Error("zap_logger_demo3", zap.Any("foo", foo{One: "one", Two: "two"}))
+	fmt.Println("----------------")
+}
+
+func zap_logger_demo4() {
+	fmt.Println("zap_logger_demo4")
+
+	type foo struct {
+		One string
+		Two string
+	}
+
+	config := zap.NewDevelopmentConfig()
+	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	logger, _ := config.Build()
+	logger.Debug("zap_logger_demo4", zap.Any("foo", foo{One: "one", Two: "two"}))
+	logger.Info("zap_logger_demo4", zap.Any("foo", foo{One: "one", Two: "two"}))
+	logger.Warn("zap_logger_demo4", zap.Any("foo", foo{One: "one", Two: "two"}))
+	logger.Error("zap_logger_demo4", zap.Any("foo", foo{One: "one", Two: "two"}))
+	fmt.Println("----------------")
+}
+
+func zap_logger_demo5() {
+	fmt.Println("zap_logger_demo5")
+
+	type foo struct {
+		One string
+		Two string
+	}
+
+	config := zap.NewDevelopmentConfig()
+	config.EncoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	logger, _ := config.Build()
+	logger.Debug("zap_logger_demo5", zap.Any("foo", foo{One: "one", Two: "two"}))
+	logger.Info("zap_logger_demo5", zap.Any("foo", foo{One: "one", Two: "two"}))
+	logger.Warn("zap_logger_demo5", zap.Any("foo", foo{One: "one", Two: "two"}))
+	logger.Error("zap_logger_demo5", zap.Any("foo", foo{One: "one", Two: "two"}))
+	fmt.Println("----------------")
 }
